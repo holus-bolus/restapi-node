ddb63794b013c0a243f74f332a435e7f5135fad6
holus-bolus <ustinovoleksij@gmail.com>
2024-02-05 12:03:59 +0200

    Added the post route

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const placesRoutes = require('./routes/places-routes');
+const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use('/api/places', placesRoutes);
-app.listen(5000, () => {
-    console.log('Server is running on port 5000.')
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(err.code || 500);
+  res.json({ message: err.message || "An unknown error occurred!" });
 });
-``
+
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000.");
+});
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,26 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://www.planetware.com/photos-large/USNY/empire-state-building.jpg',
-        address: '20 W 34th St, New York, NY 10118',
-        location: {lat: 40.748817, lng: -73.985428},
-        creator: 'u1'
-    }
-];
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
-    console.log('GET request in places');
-    res.json({place});
-});
+const HttpError = require("../models/http-error");
 
+const placesControllers = require("../controllers/places-controllers");
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
+
+router.post("/", placesControllers.createPlace);
 
 module.exports = router;
