b8670d154e84a1bbe561e32ef7ca2cba57ca42b2
holus-bolus <ustinovoleksij@gmail.com>
2024-02-05 08:50:45 +0200

    Added the restapi

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000.')
+});
